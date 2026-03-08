4b4082940f39aca90abbd03efa9e5c9398ee05d8
carlos <cwcasado@gmail.com>
2026-03-08 17:52:14 -0300

    finalizado por enquanto

--- ex38_ad_txt.py
+++ ex38_ad_txt.py
@@ -1,2 +1,2 @@
-with open(r"C:\Users\cw_c4\Desktop\Algoritmos\semana1\Exemplo.txt", "a", encoding="utf-8") as arquivo:
+with open(r"C:\Users\cw_c4\Desktop\Algoritmos\lista 1\Exemplo.txt", "a", encoding="utf-8") as arquivo:
     arquivo.write("\nMais uma linha adicionada com sucesso!")
